e1750aedc7830e39bbd04e1514ac01e2c9e2685a
naracodes <naralee0124@gmail.com>
2020-06-03 10:58:37 -0400

    Cmplete Error Handling Funtime w4d3

--- aa_classwork/W4D2/super_useful.rb
+++ aa_classwork/W4D2/super_useful.rb
@@ -1,53 +1,73 @@
-# PHASE 2
-def convert_to_int(str)
-  begin
-    Integer(str)
-  rescue ArgumentError => e
-    puts "Cannot convert to integer"
-    puts "Error was : #{e.message}"
-  end
-end
+# # PHASE 2
+# def convert_to_int(str)
+#   begin
+#     Integer(str)
+#   rescue ArgumentError => e
+#     puts "Cannot convert to integer"
+#     puts "Error was : #{e.message}"
+#   end
+# end
 
-# p convert_to_int("hello")
+# # p convert_to_int("hello")
 
-class CoffeeFound < StandardError
-end
+# class CoffeeFound < StandardError
+# end
 
-# PHASE 3
-FRUITS = ["apple", "banana", "orange"]
+# # PHASE 3
+# FRUITS = ["apple", "banana", "orange"]
 
-def reaction(maybe_fruit)
-  if FRUITS.include? maybe_fruit
-    puts "OMG, thanks so much for the #{maybe_fruit}!"
-  elsif maybe_fruit == "coffee"
-    raise CoffeeFound
-  else
-    raise StandardError
-  end
-end
+# def reaction(maybe_fruit)
+#   if FRUITS.include? maybe_fruit
+#     puts "OMG, thanks so much for the #{maybe_fruit}!"
+#   elsif maybe_fruit == "coffee"
+#     raise CoffeeFound
+#   else
+#     raise StandardError
+#   end
+# end
 
-# p reaction("mango")
+# # p reaction("mango")
 
-def feed_me_a_fruit
-    puts "Hello, I am a friendly monster. :)"
-  begin
-    puts "Feed me a fruit! (Enter the name of a fruit:)"
-    maybe_fruit = gets.chomp
-    reaction(maybe_fruit)
-  rescue CoffeeFound => e
-    puts e.message
-  retry
-  end
-end
+# def feed_me_a_fruit
+#     puts "Hello, I am a friendly monster. :)"
+#   begin
+#     puts "Feed me a fruit! (Enter the name of a fruit:)"
+#     maybe_fruit = gets.chomp
+#     reaction(maybe_fruit)
+#   rescue CoffeeFound => e
+#     puts e.message
+#   retry
+#   end
+# end
 
-p feed_me_a_fruit
+# p feed_me_a_fruit
+
+
 
 # PHASE 4
+
+class NotBestyError < StandardError
+end
+
+class NotEnoughError < StandardError
+end
+
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
-    @name = name
-    @yrs_known = yrs_known
-    @fav_pastime = fav_pastime
+    begin
+      if name.length <= 0 || fav_pastime.length <= 0
+        raise NotEnoughError
+      end
+      @name = name
+      raise NotBestyError unless yrs_known >= 5
+      @yrs_known = yrs_known
+      @fav_pastime = fav_pastime
+    rescue NotBestyError
+      puts "Years known must be greater than or equal to 5"
+    rescue NotEnoughError
+      puts "Name and favorite past time should be passed in!"
+    end
+
   end
 
   def talk_about_friendship
@@ -63,4 +83,5 @@ class BestFriend
   end
 end
 
+a=BestFriend.new("blah", 5, "")
 

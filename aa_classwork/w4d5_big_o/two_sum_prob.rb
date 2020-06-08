9ae76875c63595c276defc972bc34a03fdea524f
naracodes <naralee0124@gmail.com>
2020-06-08 11:34:50 -0400

    Work on w4d5 classwork

---
+++ aa_classwork/w4d5_big_o/two_sum_prob.rb
@@ -0,0 +1,12 @@
+# Learning Goals
+# Be able to reason about a difficult problem and think of ways to solve it.
+# Be able to determine and explain the time and space complexity of a method
+# Be able to recognize when time or space complexity can be improved
+# Know some common tricks to improve time or space complexity
+
+
+
+
+arr = [0, 1, 5, 7]
+two_sum?(arr, 6) # => should be true
+two_sum?(arr, 10) # => should be false

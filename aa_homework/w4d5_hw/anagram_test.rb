a03e86a508105ecf9aeaedeea464f789360ea4fd
naracodes <naralee0124@gmail.com>
2020-06-07 21:49:30 -0400

    Add w5d1 lru cache hw

--- aa_homework/w4d5_hw/anagram_test.rb
+++ aa_homework/w4d5_hw/anagram_test.rb
@@ -1,1 +1,21 @@
 require 'benchmark'
+
+def my_anagram(str1, str2)
+    run = Time.now
+    str1.length == str2.length && (str1.chars - str2.chars)
+    run - Time.now
+end
+
+
+
+def hash(str)
+    count = Hash.new(0)
+    str.each_char { |char| count[char] += 1 }
+    count
+end
+
+def hash_anagram(str1, str2)
+    hash(str1) == hash(str2)
+end
+
+puts my_anagram("abcdefghijklmnop", "ponmlkjihgfedcba")

bae7ad867ced01d24f0feba9ae93e0ca2c72e405
naracodes <naralee0124@gmail.com>
2020-07-05 22:22:54 -0400

    Commit Canvas hw w9d1

---
+++ aa_homework/w9d1/canvas_practice/practice.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const myCanvas = document.getElementById("mycanvas");
+    myCanvas.height = 500;
+    myCanvas.width = 500;
+    
+    const ctx = myCanvas.getContext("2d");
+    ctx.fillStyle = "#c77070";
+    ctx.fillRect(0, 0, 500, 500);
+
+    ctx.beginPath()
+    ctx.arc(100, 100, 20, 0, 2 * Math.PI, true);
+    ctx.strokeStyle = "#607d8b";
+    ctx.lineWidth = 40;
+    ctx.stroke();
+
+});
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const canvasEl = document.getElementById("mycanvas");
+//     canvasEl.width = 500;
+//     canvasEl.height = 500;
+
+//     const ctx = canvasEl.getContext("2d");
+//     ctx.fillStyle = "purple";
+//     ctx.fillRect(0, 0, 500, 500);
+
+//     ctx.beginPath();
+//     ctx.arc(100, 100, 20, 0, 2 * Math.PI, true);
+//     ctx.strokeStyle = "green";
+//     ctx.lineWidth = 5;
+//     ctx.stroke();
+//     ctx.fillStyle = "blue";
+//     ctx.fill();
+// });
